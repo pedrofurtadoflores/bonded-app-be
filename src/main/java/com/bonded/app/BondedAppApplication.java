1442abca96f951ba12c286fa510696a0a48dc2c7
Pedro <pedrofurtadoflores@gmail.com>
2025-01-27 20:49:44 -0300

    bonded-app first commit

---
+++ src/main/java/com/bonded/app/BondedAppApplication.java
@@ -0,0 +1,13 @@
+package com.bonded.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondedAppApplication.class, args);
+	}
+
+}
